41e5ce7b2ca0c212922f7674c64011af590571fe
TanyaBelova <tanya.belova19@yandex.ru>
2020-03-02 21:41:14 +0300

    Класс с unit-тестами

--- src/main/java/com/javamaster/tatyana/CustomFieldException.java
+++ src/main/java/com/javamaster/tatyana/CustomFieldException.java
@@ -1,12 +1,11 @@
 package com.javamaster.tatyana;
 
-class CustomFieldException extends Exception {
-        private String message;
+public class CustomFieldException extends Exception{
+    private String message;
 
-        public CustomFieldException(String message)
-        {
-            this.message=message;
-        }
+    public CustomFieldException(String message) {
+        this.message = message;
+    }
 
     public String getMessage() {
         return message;
